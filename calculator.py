ebcc8eb3e069e8b37e62f84c424ed881bb271f8c
Samiksharajput10 <rupeshkumarsingh1026@gmail.com>
2026-06-14 15:45:55 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+#There are some arithmetic operations 
+
+a = int(input())
+b = int(input())
+
+
+print("Addition:",a+b)
+print("Difference:",a-b)
+print("Product:",a*b)
+print("Division:",a/b)
+
+
